5c003ed940bff2e8fd100e51e0b1f88ab47155cf
Unknown <pacome.chulejeune@viacesi.fr>
2019-04-09 17:08:19 +0200

    XorCipher + KeyFinder Class
    Can encrypt and decrypt
    Can find key with frequentiel analysis

---
+++ KeyFinder.py
@@ -0,0 +1,59 @@
+import string
+from XORCipher import XORCipher
+
+class KeyFinder:
+
+    def frqcFinder(msg):
+        key = ""
+        keylen = 0
+
+        key = KeyFinder.frqcFinder(msg, keylen)
+        return key
+
+    def frqcFinder(msg, keylen):
+        key = ""
+        msgBloc = []
+        keyDict = []
+        for i in range(0, keylen):
+            msgBloc.append([])
+        for i in range(0, len(msg)):
+            msgBloc[i%keylen].append(msg[i])
+        for i in range(0, keylen):
+            letterDict = {}
+            keyDict.append({"letter": "", "value": 0})
+            for letter in string.ascii_lowercase:
+                # letterDict[letter] = []
+                decryptBloc = []
+                for j in msgBloc[i]:
+                    decryptBloc.append(XORCipher.xorCiphering(j, letter))
+                letterDict[letter] = decryptBloc.count('e')+1/len(decryptBloc)
+                if(keyDict[i] is None or keyDict[i]["value"] < letterDict[letter]):
+                    keyDict[i] = {"letter": letter, "value": letterDict[letter]}
+            key += keyDict[i].get("letter")
+
+        # msgBloc = [msg[i:i+keylen] for i in range(0, len(msg), keylen)]
+
+        return key
+
+    def hammingDistance(bloc1, bloc2):
+        hammingDistance = 0
+        return hamminDistance
+
+
+#file1 = "PB"
+files = ["PA", "PB", "PC", "PD", "PE", "PF", "PG", "PH", "PI", "PJ", "PK"]
+
+for i in range(len(files)):
+
+    message = XORCipher.getTxtFrmFile("C:\\Users\\pblue\\Downloads\\Groupe 1 (3)\\{}.txt".format(files[i]))
+
+    key = KeyFinder.frqcFinder(message, 6)
+    print("{}: {}".format(files[i], key))
+
+#decrypted = XORCipher.xorCiphering(message, key)
+
+#fileToWrite = XORCipher.printTxtToFile("C:\\Users\\pblue\\Downloads\\Groupe 1\\decrypted\\{}test.txt".format(file1), decrypted)
+    
+
+
+
---
+++ XORCipher.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import os
+import codecs
+import numpy as np
+
+fileEncoding = 'iso8859_1'
+
+class XORCipher:
+    instances = 0
+    def __init__(self, file, key):
+        self.file = file
+        self.key = key
+        Cypherer.instances += 1
+
+
+    def xorCiphering(msg, key):
+        decodedMsg = ""
+        for i in range(len(msg)):
+            r = ord(msg[i]) ^ ord(key[i%len(key)])
+            decodedMsg += chr(r)
+        return decodedMsg
+
+    def getTxtFrmFile(location):
+        fileToDecrypt = codecs.open(location, encoding=fileEncoding , errors='ignore')
+        message = fileToDecrypt.read()
+        fileToDecrypt.close()
+        return message
+
+    def printTxtToFile(location, msg):
+        fileToDecrypt = codecs.open(location, mode="w", encoding=fileEncoding , errors='ignore')
+        fileToDecrypt.write(msg)
+        fileToDecrypt.close()
+
+
+
+# c1 = XORCipher("", "")
+
+# files = ["PA", "PB", "PC", "PD", "PE", "PF", "PG", "PH", "PI", "PJ", "PK"]
+# key = "test"
+
+# for i in range(len(files)):
+
+#     message = XORCipher.getTxtFrmFile("C:\\Users\\pblue\\Downloads\\Groupe 1 (3)\\{}.txt".format(files[i]))
+#     decrypted = XORCipher.xorCiphering(message, key)
+
+#     fileToWrite = XORCipher.printTxtToFile("C:\\Users\\pblue\\Downloads\\Groupe 1\\decrypted\\{}.txt".format(files[i]), decrypted)
+    
+   
+
+
+print('end')
